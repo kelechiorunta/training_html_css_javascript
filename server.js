dcd5df2398751e27f4d43d82339dfd42cb06eb5d
Kelechi Orunta <kelechiorunta1@gmail.com>
2026-05-06 10:38:36 +0100

    install nodemon

--- server.js
+++ server.js
@@ -1,12 +1,22 @@
 import express from "express";
 import bodyParser from "body-parser";
+import path from "path";
 import { createServer } from "http";
 
 const app = express();
 
-app.use(bodyParser.urlencoded({ extended: false }));
+// Serve static files such as css and images
+app.use(express.static(path.join(".", ".")));
+
+//
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(express.json());
+
+// app.get("/", (req, res) => {
+//   const filePath = path.resolve(".", "index.html");
+//   res.sendFile(filePath);
+// });
 
 app.post("/user", (req, res) => {
   const { name, email } = req.body;
@@ -17,6 +27,6 @@ const server = createServer(app);
 
 server.listen(3650, () => console.log("Server is listening at PORT 3650"));
 
-export { app }
+export { app };
 
-export default server
+export default server;
