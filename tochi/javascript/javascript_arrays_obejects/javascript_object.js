10e50c05fb944d582d30e882cab369a4e1396e13
Kelechi Orunta <kelechiorunta1@gmail.com>
2026-06-17 09:59:26 +0100

    feat(object): updated object.js

--- tochi/javascript/javascript_arrays_obejects/javascript_object.js
+++ tochi/javascript/javascript_arrays_obejects/javascript_object.js
@@ -1,28 +1,28 @@
-const person = {
-    name: "John",
-    age: 30,
-    city: "New York",
-    fullName: function() {
-        return `${this.name} ${this.age}`;
-    }
+const person1 = {
+  name: "John",
+  age: 30,
+  city: "New York",
+  fullName: function () {
+    return `${this.name} ${this.age}`;
+  },
 };
-console.log(person);
-console.table(person);
-console.log(person.name);
-console.log(person.fullName);
-console.log(person.fullName());
+console.log(person1);
+console.table(person1);
+console.log(person1.name);
+console.log(person1.fullName);
+console.log(person1.fullName());
 
 // Task... Create an object inside name object with properties firstName and lastName, and a method fullName that returns the full name of the person.
 const person = {
-    name: {
-        firstName: "John", 
-        lastName: "Doe"
-    },
-    age: 30,
-    city: "New York",
-    fullName: function() {
-        return `${this.name.firstName} ${this.name.lastName}`;
-    }
+  name: {
+    firstName: "John",
+    lastName: "Doe",
+  },
+  age: 30,
+  city: "New York",
+  fullName: function () {
+    return `${this.name.firstName} ${this.name.lastName}`;
+  },
 };
 console.log(person);
 console.table(person);
