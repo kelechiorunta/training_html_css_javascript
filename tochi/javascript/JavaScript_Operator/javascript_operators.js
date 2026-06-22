df44786536efd47c8f935f562e3ae5d9c5e548a8
Tochukwu Ibeagwa <tochi.ibeagwa@appworld.ng>
2026-06-22 21:57:03 +0100

    class exercise on operator

---
+++ tochi/javascript/JavaScript_Operator/javascript_operators.js
@@ -0,0 +1,87 @@
+// Assignment Operator
+let name = "Tochi";
+console.log(name);
+
+
+// arithmetic operators 
+
+// Addition Operator
+let x = 5;
+let y = 10;
+let z = x + y;
+console.log(z);
+
+// Subtraction Operator
+let a = 15;
+let b = 5;
+let c = a - b;
+console.log(c);
+
+// Multiplication Operator
+let m = 4;
+let n = 6;
+let p = m * n;
+console.log(p);
+
+// Division Operator
+let q = 20;
+let r = 4;
+let s = q / r;
+console.log(s);
+
+// Modulus Operator
+let t = 10;
+let u = 3;
+let v = t % u;
+console.log(v); 
+
+// Increment Operator
+let i = 5;
+i++;
+i+= 1;
+console.log(i);
+
+// logic operators
+let isRaining = true;
+let isSunny = false;
+
+// AND operator
+let areWeHavingAGoodDay = isRaining && isSunny;
+console.log(areWeHavingAGoodDay);
+
+// OR operator
+let areWeHavingALousyDay = isRaining || isSunny;
+console.log(areWeHavingALousyDay);
+
+// comparison operators
+let num1 = 10;
+let num2 = 20;
+let isNum1Greater = num1 > num2;
+console.log(isNum1Greater);
+
+let isNum1Less = num1 < num2;
+console.log(isNum1Less);
+
+
+
+// strict equality operator
+let num3 = 10;
+let num4 = "10";
+let isNum3EqualToNum4 = num3 === num4;
+console.log(isNum3EqualToNum4);
+
+// less strict equality operator
+let isNum3EqualToNum4Loose = num3 == num4;
+console.log(isNum3EqualToNum4Loose);
+
+// && (AND) operator
+let isAdult = true;
+let hasID = true;
+let canEnterClub = isAdult && hasID;
+console.log(canEnterClub); 
+
+// || (OR) operator
+let isSunny = true;
+let isCloudy = false;
+let isWeatherNice = isSunny || isCloudy;
+console.log(isWeatherNice);
