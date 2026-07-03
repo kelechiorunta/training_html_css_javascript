ac0b97366b50d93ea887fb96c4f883775474decd
Tochukwu Ibeagwa <tochi.ibeagwa@appworld.ng>
2026-07-03 08:32:39 +0100

    Exercise on Function naming Argument and Parameter

---
+++ tochi/javascript/javaScript_function/function.js
@@ -0,0 +1,24 @@
+
+// naming a function
+function printMe(){
+    console.log("Hello World");
+}
+printMe();
+
+// Parameters and Arguments
+function printValues(name, age) {
+    console.log("Name: " + name);
+    console.log("Age: " + age);
+}
+printValues("John", 25);
+
+const printMeAGain = function() {
+    console.log("Hello Again!");
+}
+printMeAGain();
+
+const printValuesAgain = function(name, age) {
+    console.log("Name: " + name);
+    console.log("Age: " + age);
+}
+printValuesAgain("Jane", 30);
