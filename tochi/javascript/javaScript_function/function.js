aa20768d975c25763b0e85a667bc82912eab09c3
Tochukwu Ibeagwa <tochi.ibeagwa@appworld.ng>
2026-07-09 06:39:04 +0100

    Study on Function in JavaScript

--- tochi/javascript/javaScript_function/function.js
+++ tochi/javascript/javaScript_function/function.js
@@ -22,3 +22,46 @@ const printValuesAgain = function(name, age) {
     console.log("Age: " + age);
 }
 printValuesAgain("Jane", 30);
+
+function greet(name) { 
+  console.log("Hello, " + name + "!");
+}
+
+greet("Tochi"); 
+
+// returning values from a function
+function addNumbers(a, b) {
+    return a + b;
+}
+let result = addNumbers(5, 10);
+console.log(result);
+
+function multiplyNumbers(a, b) {
+    return a * b;
+}
+let product = multiplyNumbers(5, 10);
+console.log(product);
+
+function divideNumbers(a, b) {
+    if (b === 0) {
+        console.log("Cannot divide by zero");
+        return null;
+    }   
+}
+
+// Arrow Functions
+const add = (a, b) => a + b;
+console.log(add(5, 10));
+
+
+const subtract = (a, b) => {
+  return a - b;
+};
+
+
+const square = (n) => n * n;
+
+console.log(subtract(10, 4)); // 6
+console.log(square(5));       // 25
+
+
