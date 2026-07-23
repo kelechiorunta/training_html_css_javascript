3bed98fbd3b205a22ebee02f1745710613886df3
Kelechi Orunta <kelechiorunta1@gmail.com>
2026-07-23 12:18:32 +0100

    feat(assessment): pre assessment for event

---
+++ tochi/javascript/JavaScript_Event/PreAssessment.md
@@ -0,0 +1,12 @@
+Create a webpage containing:
+
+A button
+A paragraph
+
+Using addEventListener():
+
+Change the paragraph text when the button is clicked.
+Change the button background color.
+Log a message to the browser console.
+
+Explain each step of your solution.
--- tochi/javascript/JavaScript_Event/javascript_event.js
+++ tochi/javascript/JavaScript_Event/javascript_event.js
@@ -1,12 +1,19 @@
-
-
-document.addEventListener('DOMContentLoaded', function() {
-    const div = document.querySelector('div');
-    div.addEventListener("click", function() {
-        alert('Div clicked!');
-    },{capture: true});
-    const myButton = document.getElementById('myButton');
-    myButton.addEventListener('click', function() {
-        alert('Button clicked!');
-    },{bubbles: false});
+document.addEventListener("DOMContentLoaded", function () {
+  const div = document.querySelector("div");
+  div.addEventListener(
+    "click",
+    function () {
+      alert("Div clicked!");
+    },
+    // { capture: true },
+  );
+  const myButton = document.getElementById("myButton");
+  myButton.addEventListener(
+    "click",
+    function (event) {
+      alert("Button clicked!");
+      event.stopPropagation();
+    },
+    { capture: false },
+  );
 });
